516e1e258ea89523a251e3e1f1dd9debbf10f846
Rowdy Goat <dbrockman28@gmail.com>
2020-10-23 16:22:40 -0500

    added custom styling for buttons, drag, and trash

--- assets/js/script.js
+++ assets/js/script.js
@@ -117,6 +117,22 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
+  // Add and remove classes for dropover effects
+  activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function(event) {
+    $(event.target).addClass("dropover-active")
+  },
+  out: function(event) {
+    $(event.target).removeClass("dropover-active")
+  },
+
   update: function(event) {
     // Store task data in array
     let tempArr = [];
@@ -174,8 +190,15 @@ setInterval(function() {
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
+  over: function(event) {
+    $("bottom-trash").addClass("bottom-trash-drag");
+  },
+  out: function(event) {
+    $("bottom-trash").removeClass("bottom-trash-drag");
+  },
   drop: function(event, ui) {
     ui.draggable.remove();
+    $("bottom-trash").removeClass("bottom-trash-drag");
   },
 });
 
@@ -196,7 +219,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
